0408172ccfb918934802fc84a7c3547ce2229f52
N33ttu <yurifreire878@gmail.com>
2021-11-20 09:02:26 -0300

    Update cardapio.java

---
+++ cardapio/cardapio.java
@@ -0,0 +1,15 @@
+package cardapio;
+
+public class cardapio {
+    private int numero;
+
+    public void primeiraAba(){
+        System.out.println("\n ========================================");
+        System.out.println("   |      Escolha uma das duas opções:    |");
+        System.out.println("   |                                      |");
+        System.out.println("   |       1- Cardápio                    |");
+        System.out.println("   |       2- Histórico de pedidos        |");
+        System.out.println("   |                                      |");
+        System.out.println("\n ========================================");
+        
+}

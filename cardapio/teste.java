08d68c757fd4762216760c853752a376314a40d0
N33ttu2 <90645003+N33ttu2@users.noreply.github.com>
2022-03-14 15:57:03 -0300

    Update teste.java

--- cardapio/teste.java
+++ cardapio/teste.java
@@ -6,27 +6,43 @@ public class teste {
     public static void main(String args[]) {
 
         char op;
+        char lc;
 
     do {
 
         String menu;
         menu = " Escolha um pastel:";
-        menu += "\n1. 01- Queijo c/ Presunto ";
-        menu += "\n2. 02- Queijo c/ calabresa";
-        menu += "\n3. 03- Queijo c/ goiabada";
-        menu += "\n4. 04 - Queijo ";
-        menu += "\n5. 05 - Banana real";
+        menu += "\n1. - Queijo c/ Presunto " ;
+        menu += "\n2. - Queijo c/ calabresa";
+        menu += "\n3. - Queijo c/ goiabada";
+        menu += "\n4. - Queijo ";
+        menu += "\n5. - Banana real";
         menu += "\n6. SAIR";
         op = JOptionPane.showInputDialog(menu).charAt(0);
 
         switch (op) {
 
             case '1':
-            
 
+            String men;
+
+            men = "custará R$ 6,00 confirma a compra ? ";
+            men += "\n01. Sim";
+            men += "\n02. não";
+            lc = JOptionPane.showInputDialog(men).charAt(0);
+
+            switch (lc) {
+
+                case '1':
+
+                JOptionPane.showMessageDialog(null, "favor após o fechemantos se direcionar ao atendente");
+
+                } 
+ 
     } while(op != '6');
-}
+} while (op != '2');
 
 }
 
+}
 

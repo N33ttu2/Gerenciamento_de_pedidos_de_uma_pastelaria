2c31d5a938d52bbd20a72f47dfdc3e66c1c796d8
N33ttu2 <90645003+N33ttu2@users.noreply.github.com>
2022-03-15 13:32:05 -0300

    Classe final
     Será enviada para o professor como trabalho final.

---
+++ Final/Final1.java
@@ -0,0 +1,229 @@
+package Final;
+
+import javax.swing.JOptionPane;
+
+public class Final1 {
+
+    public static void main(String args[]) {
+
+        char op;
+        char lc;
+
+    do {
+
+        String menu;
+        menu = " Escolha um pastel:";
+        menu += "\n1. - Queijo c/ Presunto " ;
+        menu += "\n2. - Queijo c/ calabresa";
+        menu += "\n3. - Queijo c/ goiabada";
+        menu += "\n4. - Queijo ";
+        menu += "\n5. - Banana real";
+        menu += "\n6. - Frango c/ Queijo   ";
+        menu += "\n7. - Frango c/ Catupiry";
+        menu += "\n8  - Frango c/ Cheddar";
+        menu += "\n9. - Carne c/ Queijo  ";
+        menu += "\n0. Sair";
+        op = JOptionPane.showInputDialog(menu).charAt(0);
+
+        switch (op) {
+
+            case '1':
+
+            String men;
+
+            men = "custará R$ 6,00 confirma a compra ? ";
+            men += "\n01. Sim";
+            men += "\n02. não";
+            lc = JOptionPane.showInputDialog(men).charAt(0);
+
+            switch (lc) {
+
+                case '1':
+
+                JOptionPane.showMessageDialog(null, "favor após o fechemantos se direcionar ao atendente");
+
+                } 
+ 
+    } while(op != '0');
+
+    
+    switch (op) {
+
+        case '2':
+
+        String men;
+
+        men = "custará R$ 6,00 confirma a compra ? ";
+        men += "\n01. Sim";
+        men += "\n02. não";
+        lc = JOptionPane.showInputDialog(men).charAt(0);
+
+        switch (lc) {
+
+            case '1':
+
+            JOptionPane.showMessageDialog(null, "favor após o fechemantos se direcionar ao atendente");
+
+            } 
+
+
+
+} while (op != '2');
+
+
+switch (op) {
+
+    case '3':
+
+    String men;
+
+    men = "custará R$ 6,00 confirma a compra ? ";
+    men += "\n01. Sim";
+    men += "\n02. não";
+    lc = JOptionPane.showInputDialog(men).charAt(0);
+
+    switch (lc) {
+
+        case '1':
+
+        JOptionPane.showMessageDialog(null, "favor após o fechemantos se direcionar ao atendente");
+
+        } 
+
+
+} while (op != '2');
+
+switch (op) {
+
+    case '4':
+
+    String men;
+
+    men = "custará R$ 6,00 confirma a compra ? ";
+    men += "\n01. Sim";
+    men += "\n02. não";
+    lc = JOptionPane.showInputDialog(men).charAt(0);
+
+    switch (lc) {
+
+        case '1':
+
+        JOptionPane.showMessageDialog(null, "favor após o fechemantos se direcionar ao atendente");
+
+        } 
+
+} while (op != '2');
+
+switch (op) {
+
+    case '5':
+
+    String men;
+
+    men = "custará R$ 6,00 confirma a compra ? ";
+    men += "\n01. Sim";
+    men += "\n02. não";
+    lc = JOptionPane.showInputDialog(men).charAt(0);
+
+    switch (lc) {
+
+        case '1':
+
+        JOptionPane.showMessageDialog(null, "favor após o fechemantos se direcionar ao atendente");
+
+        } 
+
+} while (op != '2');
+
+switch (op) {
+
+    case '6':
+
+    String men;
+
+    men = "custará R$ 6,00 confirma a compra ? ";
+    men += "\n01. Sim";
+    men += "\n02. não";
+    lc = JOptionPane.showInputDialog(men).charAt(0);
+
+    switch (lc) {
+
+        case '1':
+
+        JOptionPane.showMessageDialog(null, "favor após o fechemantos se direcionar ao atendente");
+
+        } 
+
+}while (op != '2'); 
+
+switch (op) {
+
+    case '7':
+
+    String men;
+
+    men = "custará R$ 6,00 confirma a compra ? ";
+    men += "\n01. Sim";
+    men += "\n02. não";
+    lc = JOptionPane.showInputDialog(men).charAt(0);
+
+    switch (lc) {
+
+        case '1':
+
+        JOptionPane.showMessageDialog(null, "favor após o fechemantos se direcionar ao atendente");
+
+        } 
+
+} while (op != '2');
+
+switch (op) {
+
+    case '8':
+
+    String men;
+
+    men = "custará R$ 6,00 confirma a compra ? ";
+    men += "\n01. Sim";
+    men += "\n02. não";
+    lc = JOptionPane.showInputDialog(men).charAt(0);
+
+    switch (lc) {
+
+        case '1':
+
+        JOptionPane.showMessageDialog(null, "favor após o fechemantos se direcionar ao atendente");
+
+        } 
+
+} while (op != '2');
+
+switch (op) {
+
+    case '9':
+
+    String men;
+
+    men = "custará R$ 6,00 confirma a compra ? ";
+    men += "\n01. Sim";
+    men += "\n02. não";
+    lc = JOptionPane.showInputDialog(men).charAt(0);
+
+    switch (lc) {
+
+        case '1':
+
+        JOptionPane.showMessageDialog(null, "favor após o fechemantos se direcionar ao atendente");
+
+        } 
+
+} while (op != '2');
+
+
+    } while (op != '0');
+
+}
+
+}
+    
+
